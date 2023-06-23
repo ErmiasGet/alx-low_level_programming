2fc3b46ab5dee61a99a3139da03f3a9ce4316d68
ErmiasGeta <programmerabe8@gmail.com>
2023-06-23 12:51:04 -0700

    1-isdigit.c

---
+++ 1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check if a letter is digit
+ * @x : The number to be checked
+ * return: 1 for character will be a digit or 0 for any else
+ */
+int _isdigit(int c)
+{
+	if (x >= 48 && x <= 57)
+	{
+	return(1);
+        }
+	return(0);
+}
