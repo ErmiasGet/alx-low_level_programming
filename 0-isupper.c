700646b358c7f9945a6c2a5c350482734b033cfb
ErmiasGeta <programmerabe8@gmail.com>
2023-06-23 12:43:42 -0700

    0-isupper.c

--- 0-isupper.c
+++ 0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper -check if a letter is upper
+ * _isupper - check if a letter is upper
  * @x : The number to be checked
  * return: 1 for upper letter or 0 for any else
  */
@@ -8,7 +8,7 @@ int _isupper(int c)
 {
 	if (x >= 65 && x <= 90)
 	{
-		return(1);
+	return(1);
         }
 	return(0);
 }
